e84d8463ad0f2c336cb95a50c241c9e7f3a48ede
David Boling <bolingd@gmail.com>
2020-09-26 17:04:04 -0400

    Challenge 15 Complete

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,40 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+const path = require('path')
+const lipsum = require(path.join(__dirname, '..', 'lipsum.js'))
+
+const posts = []
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res) {
   res.render('index', {
+    lipsum: lipsum('index'),
+    posts: posts,
+  })
+})
 
-  });
-});
+router.get(['/contact', '/about'], (req, res) => {
+  const path = req.originalUrl.replace('/', '')
+  res.render(path, {
+    lipsum: lipsum(path),
+  })
+})
 
-module.exports = router;
+router.get('/compose', (req, res) => {
+  res.render('compose')
+})
+
+router.post('/compose', (req, res) => {
+  router.use(bodyParser.urlencoded({ extended: true }))
+  const post = {
+    /** @namespace req.body.composeTitle */
+    title: req.body.composeTitle,
+    /** @namespace req.body.composePost */
+    post: req.body.composePost,
+  }
+  posts.push(post)
+  res.redirect('/')
+})
+
+module.exports = router
